5b1613a1f5bd2600600a314d0b605b837e93a291
Florian Boehler <florian.boehler1990@gmail.com>
2023-12-27 13:33:00 +0100

    i18n

--- src/components/i18next/i18n.js
+++ src/components/i18next/i18n.js
@@ -16,7 +16,7 @@ i18next
       caches: ["cookie"],
     },
     backend: {
-      loadPath: "ipwa01/assets/local/{{lng}}/translation.json", // Path to the translation files
+      loadPath: "/assets/local/{{lng}}/translation.json", // Path to the translation files
     },
   });
 
