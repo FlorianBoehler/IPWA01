e45fd82b9803c8267f9b2fbc0e1c49b9ecd987cf
Florian Boehler <florian.boehler1990@gmail.com>
2024-01-14 10:42:18 +0100

    gh-pages

--- src/components/mainNavigation/MainNavigationData.js
+++ src/components/mainNavigation/MainNavigationData.js
@@ -3,7 +3,7 @@ import tableIcon from "../../media/icons/table.svg";
 import contactUsIcon from "../../media/icons/info-circle.svg";
 
 export const MainNavigationData = [
-  { title: "home", url: "/", img: homeIcon, alt: "Home Icon" },
+  { title: "home", url: "/ipwa01", img: homeIcon, alt: "Home Icon" },
   { title: "table", url: "/table", img: tableIcon, alt: "Table Icon" },
   { title: "contact", url: "/overus", img: contactUsIcon, alt: "Contact Us" },
 ];
